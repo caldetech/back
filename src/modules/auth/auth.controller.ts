a416d0017103ea4345d5a674e168636c33ab5600
Kelven Souza <kelven.souza.2025@gmail.com>
2025-04-01 16:26:05 -0300

    Under development

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('/sign-in')
   async signIn(
     @Body()
     { email, password }: { email: string; password: string },
   ) {
     return this.authService.signIn(email, password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -6,13 +6,24 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { FastifyRequest } from 'fastify';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly jwtService: JwtService) {}
 
+  private extractTokenFromHeader(request: FastifyRequest): string | undefined {
+    const authorization = request.headers.authorization;
+
+    if (!authorization) return undefined;
+
+    const [type, token] = authorization.split('');
+
+    return type === 'Bearer' ? token : undefined;
+  }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request: unknown = context.switchToHttp().getRequest();
+    const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
