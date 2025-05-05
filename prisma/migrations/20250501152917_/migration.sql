7392dfd347632c06f949d9f9b7dadb1d718d514f
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-05 17:19:57 -0300

    Under development

--- prisma/migrations/20250501152917_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "orders" DROP CONSTRAINT "orders_customerid_fkey";
-
--- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_customerid_fkey" FOREIGN KEY ("customerid") REFERENCES "customers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
